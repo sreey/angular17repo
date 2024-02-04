af9cb63ca929da54ddc3a49126cc2bf91ba904a1
sreey <sreeyakkanti@gmail.com>
2024-02-04 14:58:55 -0500

    Added angular material component framework

--- angular-play/src/app/app.module.ts
+++ angular-play/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
